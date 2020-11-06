8f97201380c366addb9c9959412ae68ab4a97406
Eary, Sam <Eary.Sam@aoins.com>
2020-11-06 16:42:38 -0500

    New icons

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import Vue from "vue";
-import { BootstrapVue, BIcon, BIconGearFill, BIconPlus } from "bootstrap-vue";
+import {
+  BootstrapVue,
+  BIcon,
+  BIconGearFill,
+  BIconPlus,
+  BIconPencil,
+  BIconPencilFill
+} from "bootstrap-vue";
 import { initialLoad, store } from "./store/store";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
@@ -11,6 +18,8 @@ Vue.use(BootstrapVue);
 Vue.component("BIcon", BIcon);
 Vue.component("BIconGearFill", BIconGearFill);
 Vue.component("BIconPlus", BIconPlus);
+Vue.component("BIconPencil", BIconPencil);
+Vue.component("BIconPencilFill", BIconPencilFill);
 
 initialLoad();
 
